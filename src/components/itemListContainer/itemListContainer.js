29a4e8b56bbd7bcfc6003ef75900b6d1a1e7f699
Maeruth22 <72513305+Maeruth22@users.noreply.github.com>
2021-07-22 14:17:49 -0300

    Add files via upload

--- src/components/itemListContainer/itemListContainer.js
+++ src/components/itemListContainer/itemListContainer.js
@@ -1,8 +1,17 @@
-export const ItemListContainer = () => {
+import './itemListContainer.css';
+import '../itemCount/itemCount';
+import ItemCount from '../itemCount/itemCount';
+import ItemList from './ItemList/ItemList';
+import Item from './ItemList/Item';
+export const ItemListContainer = (props) => {
+    console.log(props);
+    
     return (
     <div>
-        <h2>Bienvenidx a Catfour!</h2>
-
+        
+        <h2>{props.titulo}</h2>
+        <ItemCount />
+        <ItemList />
     </div>
     )
 }
