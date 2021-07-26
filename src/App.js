8f7d6bf91804cd9ad79f260d089a7c4df013852f
Maeruth22 <72513305+Maeruth22@users.noreply.github.com>
2021-07-25 21:13:33 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import './components/navbar/navbar';
 import {Navbar} from './components/navbar/navbar';
 import { ItemListContainer } from './components/itemListContainer/itemListContainer';
-
+import ItemDetailContainer from './components/itemDetailContainer/itemDetailContainer';
+import {ItemDetail} from './components/itemDetailContainer/itemDetail';
+//import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-          <Navbar />
-          <ItemListContainer />
-    </div>
+   <>
+      <Navbar />
+      <ItemListContainer titulo="Bienvenido a Catfour" />
+      <ItemDetailContainer />
+          </>
   );
 }
 
