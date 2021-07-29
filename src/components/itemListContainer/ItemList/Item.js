bf69be133783a612c0bf9953e47d899ad93c59b4
Maeruth22 <72513305+Maeruth22@users.noreply.github.com>
2021-07-28 22:51:56 -0300

    Add files via upload

--- src/components/itemListContainer/ItemList/Item.js
+++ src/components/itemListContainer/ItemList/Item.js
@@ -1,14 +1,28 @@
 import { useState } from "react"
-
+import {Card, Row, Col, Button} from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
+import './Item.css'
 
 export default function Item (props) {
     return (
-        <>
-        <div>
-            <h1>{props.title}</h1>
-            <div>{props.description}</div>
-            <div>{props.price}</div>
-        </div>
-       </>
+        <div className='container col-md-4'>
+            <Row className='Row'>
+                
+                <Col>
+                    <Card className='card'>
+                        <Card.Body>
+                            <Card.Img src={props.img} fluid/>
+                            <Card.Title>{props.title}</Card.Title>
+                            <Card.Text>{props.description}</Card.Text>
+                            <Card.Text>{props.price}</Card.Text>
+                                <Button variant="warning">
+                                    <NavLink to="/item">Ver Producto</NavLink>
+                                </Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                
+            </Row>
+       </div>
     )
     }
--- src/components/itemListContainer/ItemList/ItemList.js
+++ src/components/itemListContainer/ItemList/ItemList.js
@@ -1,14 +1,22 @@
 import {useState, useEffect} from 'react';
 import Item from './Item'
+import {useParams} from 'react-router-dom';
+
 
 export default function ItemList () {
-    const [itemList, setItemList] = useState ([])
+    const [itemList, setItemList] = useState ([]);
+
+    const {categoryId} = useParams();
     
     useEffect(() => {
         const items = [
-            {id: 'rascador' , title: 'Rascador' , description: 'Para que rasque en vez de los muebles' , price: 1000},
-            {id: 'torre' , title: 'Torre' , description: 'Para que mire toda la casa', price: 3000},
-            {id: 'transportador' , title:'Transportador' , description: 'Para llevarlo a todos lados' , price: 2000}
+            {id: 'rascador' , title: 'Rascador' , description: 'Para que rasque en vez de los muebles' , price: 1000, category:'rascadores' , url: 'https://cdn.pixabay.com/photo/2017/01/31/19/04/cat-2026479_960_720.jpg'},
+            {id: 'torre' , title: 'Torre' , description: 'Para que mire toda la casa', price: 3000, category:'rascadores' , url:'https://cdn.pixabay.com/photo/2019/11/25/09/11/cat-4651385_960_720.jpg'},
+            {id: 'transportador' , title:'Transportador' , description: 'Para llevarlo a todos lados' , price: 2000, category:'transportadores' , url:'https://cdn.pixabay.com/photo/2020/04/15/16/16/cat-5047346_960_720.jpg'},
+            {id: 'fuente' , title: 'Fuente de agua' , description: 'Para que sienta que toma de un manantial' , price: 2000 , category: 'fuentes' , url:'https://media.istockphoto.com/photos/thirsty-tabby-cat-drinking-water-from-a-pet-drinking-fountain-side-picture-id1061642872?s=612x612'},
+            {id: 'bolson' , title: 'Bolson transportador' , description: 'Bolson para transladarlo' , price:1000 , category:'transportadores' , url:'https://cdn.pixabay.com/photo/2012/03/02/00/39/cat-20841_960_720.jpg'},
+            {id: 'raton', title: 'Ratón', description: 'Un ratón juguetón', price: 200, category: 'juguetes' , url: 'https://cdn.pixabay.com/photo/2019/12/26/15/00/new-years-eve-4720578_960_720.jpg' },
+        
         ]
         const getPromiseTask = () => {
             return task
@@ -30,10 +38,18 @@ export default function ItemList () {
         .catch(error => {console.log('Hubo un error')})
         .finally(console.log ('Ejecuta finally'))
     }, [])
-    console.log(itemList)
+       /*  categoryId ()
+        ? items.then (resuelto => {
+            setItemList(resuelto.filter(i => i.category === categoryId));
+        })
+        : items.then(resuelto =>{
+            setItemList(resuelto);
+        });*/
+     
+
     return (
         <div>
-         { itemList.map((item)=> <Item key={item.id} title={item.title} description={item.description} price={item.price}/> ) }
+         { itemList.map((item)=> <Item key={item.id} title={item.title} description={item.description} price={item.price} img={item.url}/> ) }
         </div>
     )
        
